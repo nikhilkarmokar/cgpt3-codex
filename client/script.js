24a584f091d73daf438abffd7c91d6a3392539f7
Nikhil Karmokar <nikhil@dhitechnologies.org>
2022-12-26 18:08:09 +0530

    url change

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://cgpt-codex.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
